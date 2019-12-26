5214e0a3a842fa44c1ff5046cac5cff271f8b523
eladitzhak <eladit@gmail.com>
2019-12-26 15:13:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(logger('dev'));
 
 app.use('/slides', slidesCtrl);
 app.use('/user', userctrl);
