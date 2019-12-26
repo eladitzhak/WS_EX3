aaff5423ad4d8202800ea6c7218e6bb0c9028707
eladitzhak <eladit@gmail.com>
2019-12-27 00:31:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/slides', slidesCtrl);
 app.use('/user', userctrl);
 
-app.get('/', (req, res) => {
+app.use('/', (req, res) => {
 	res.status(200);
 	res.send('missing path');
 });
